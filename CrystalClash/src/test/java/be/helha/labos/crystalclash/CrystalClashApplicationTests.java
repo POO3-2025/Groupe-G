7522fcaebbccd8cb616c26031971fe499753b802
celioMigliara <la217853@student.helha.be>
2025-04-09 14:58:56 +0200

    Test commit Crystal

---
+++ CrystalClash/src/test/java/be/helha/labos/crystalclash/CrystalClashApplicationTests.java
@@ -0,0 +1,13 @@
+package be.helha.labos.crystalclash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrystalClashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
